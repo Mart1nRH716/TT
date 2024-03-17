8b0cafe76eca1d3b3380f4a07b772707b5263166
Martin Romero <nahual332@outlook.com>
2024-03-17 13:19:31 -0600

    Ya quedo un login que te permite iniciar sesion y cerrarla, es basica, aun falta ver lo de alumnos y profesores

--- TT/TT/settings.py
+++ TT/TT/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'usuarios.apps.UsuariosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'usuarios.apps.UsuariosConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
