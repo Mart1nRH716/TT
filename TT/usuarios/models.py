e357f5e8e8f3f876254fbb2665c90b57cf27c7e3
Martin Romero <nahual332@outlook.com>
2024-03-17 20:26:55 -0600

    Agregue para cambiar la contraseña y tambien hice modificaciones a los modelos, sin embargo, tengo dudas con lso modelos y los roles

--- TT/usuarios/models.py
+++ TT/usuarios/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 
 class Persona(models.Model):
-    id = models.IntegerField(primary_key=True, unique=True, auto_created=True)
     nombre = models.CharField(max_length=20)
     apellido = models.CharField(max_length=20)
     edad = models.IntegerField()
@@ -15,12 +14,23 @@ class Persona(models.Model):
 
 
 class Alumno(models.Model):
-    id = models.IntegerField(primary_key=True, unique=True, auto_created=True)
+    class Carrera(models.TextChoices):
+        # Agregamos la clase para las carreras
+        # El valor que se almacena es ISC o LCD y para no confundirlo, para nosotros se mostrara como sistemas_computacionales
+        SISTEMAS = 'ISC', 'sistemas_computacionales'
+        LICDATOS = 'LCD', 'licencia_datos'
+        IA = 'IIA', 'inteligencia_artificial'
+
     num_boleta = models.CharField(unique=True, max_length=255)
-    carrera = models.CharField(max_length=50)
+    carrera = models.CharField(max_length=3, choices=Carrera.choices, default=Carrera.SISTEMAS)
     plan = models.CharField(max_length=50)
     nombre_protocolo = models.CharField(max_length=50)
     persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
+
+
+    # Campo de contraseña
+    password = models.CharField(max_length=128)
+
 
     class Meta:
         ordering = ['num_boleta']
